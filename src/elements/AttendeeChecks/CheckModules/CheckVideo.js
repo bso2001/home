a24e507351a6274057453585e989f120272ce195
bso2001 <bert.olsson@gmail.com>
2021-10-26 11:48:17 -0400

    bzc-tb -- video help working

--- src/elements/AttendeeChecks/CheckModules/CheckVideo.js
+++ src/elements/AttendeeChecks/CheckModules/CheckVideo.js
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from 'react'
 
 import { CSTYLES, resultCellStyle } from './styles'
 import { EStatus } from '../common'
+import { getDeviceInfo } from 'library/device'
 
 export const CheckVideo = ({ status, title, inColumns, onComplete }) =>
 {
 	const [started, setStarted] = useState(false)
+	const [retrying, setRetrying] = useState(false)
+	const [message, setMessage] = useState( `Do you see the video playing?` )
 
 	useEffect( () =>
 	{
@@ -15,24 +18,72 @@ export const CheckVideo = ({ status, title, inColumns, onComplete }) =>
 	}, [status])
 
 	const testPassed =()=> { onComplete( EStatus.PASSED) }
-	const testFailed =()=> { onComplete( EStatus.FAILED, { notVisible : false } ) }
-	const testFailedThumbnail =()=> { onComplete( EStatus.FAILED, { notVisible : true } ) }
+
+	const testFailed =()=>
+	{
+		if ( ! retrying ) 
+		{
+			setRetrying(true)
+			videoHelp()
+		}
+		else
+			onComplete( EStatus.FAILED, { notVisible : undefined } )
+	}
+
+	const videoHelp = () =>
+	{
+		let link, type
+		const { os } = getDeviceInfo()
+
+		switch ( os?.name )
+		{
+			case 'Android':
+				link = 'https://www.techsolutions.support.com/how-to/fix-call-or-audio-quality-android-phone-or-tablet-10740'
+				type = 'Android'
+				break
+
+			case 'iOS':
+				link = 'https://www.lifewire.com/fix-volume-on-iphone-4174470'
+				type = 'iPad or iPhone'
+				break
+
+			case 'macOS':
+				link = 'https://macpaw.com/how-to/fix-sound-on-mac'
+				type = 'Mac'
+				break
+
+			case 'Windows':
+				link = 'https://macpaw.com/how-to/fix-sound-on-mac'
+				type = 'Windows'
+				break
+
+			default:
+				link = 'https://www.drivereasy.com/knowledge/easy-to-fix-no-sound-on-computer'
+				type = ''
+		}
+
+		setMessage (`<b><u><a href=${link}
+				target="_blank">Click here</a></b></u> for information on enabling your ${type} video.` )
+	}
 
 	const noButtonStyle = { ...CSTYLES.button( inColumns ), ...CSTYLES.noButton }
 
-	return ( started &&
+	return (
 		<div style={ CSTYLES.outer( inColumns ) }>
 			<div style={{ ...resultCellStyle( inColumns ), minHeight : '15vh' }}>
 				<div style={ CSTYLES.title( inColumns ) }>{ title }</div>
-				<div style={ CSTYLES.result( inColumns ) }>Is the video playing?</div>
+				<div style={ CSTYLES.result( inColumns ) } dangerouslySetInnerHTML={{ __html : message }} />
 			</div>
 
 			<div style={ CSTYLES.cell( inColumns ) } hidden={ !started }>
-				<button style={ noButtonStyle } onClick={testFailed}>No, I don't see the video playing</button>
-				<button style={ noButtonStyle } onClick={testFailedThumbnail}>No, the video is blank</button>
-				<button style={ CSTYLES.button( inColumns ) } onClick={testPassed}>Yes, I see the video playing</button>
+
+				<button style={ noButtonStyle } onClick={testFailed} >
+					{ retrying ? `Still no video; skip test` : `No, I don't see the video playing` }
+				</button>
+				<button style={ CSTYLES.button( inColumns ) } onClick={testPassed} >
+					{ retrying ? `Yes, I see the video now` : `Yes, I see the video` }
+				</button>
 			</div>
 		</div>
 	)
 }
-
